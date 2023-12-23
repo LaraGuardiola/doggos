28a7768a4f78fec11aa680e1618405a95652099c
Sergio Lara Guardiola <webdev.slara@gmail.com>
2023-12-23 20:14:50 +0100

    Updated moe image, testing some mobile stuff

--- doggos.js
+++ doggos.js
@@ -1,9 +1,13 @@
+if (screen.orientation === 'portrait') {
+  screen.orientation.lock("portrait")
+}
+
 const counter = document.querySelector('.counter')
 let num = 0
 
 setInterval(() => {
-    num++
-    counter.innerHTML = num
+  num++
+  counter.innerHTML = num
 }, 2000)
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -18,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (isVisible && !section.classList.contains("animated")) {
         section.classList.add("animated");
-        
+
         setTimeout(() => {
           animatedImages[index].classList.add("active");
         }, 500);
@@ -31,7 +35,65 @@ document.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("scroll", handleScroll);
   window.addEventListener("resize", handleScroll);
-
-  // Llama a la función handleScroll al cargar la página para activar la animación inicial
+  //Llama a la función handleScroll al cargar la página para activar la animación inicial
   handleScroll();
+
+  let prevScrollTop = window.scrollY || document.documentElement.scrollTop;
+  let prevScrollDirection = '';
+
+  const handleScrollToSection = (direction) => {
+    const elements = document.querySelectorAll('section'); 
+
+    let currentElement = null;
+
+    elements.forEach(element => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+        currentElement = element;
+      }
+    });
+
+    if (currentElement) {
+      const currentIndex = Array.from(elements).indexOf(currentElement);
+      let targetElement = null;
+
+      if (direction === 'down' && currentIndex < elements.length - 1) {
+        targetElement = elements[currentIndex + 1];
+      } else if (direction === 'up' && currentIndex > 0) {
+        targetElement = elements[currentIndex - 1];
+      }
+
+      if (targetElement) {
+        // Scroll al siguiente o anterior elemento
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+
+  window.addEventListener('scroll', function () {
+    const st = window.scrollY || document.documentElement.scrollTop;
+    if (st > prevScrollTop && prevScrollDirection !== 'down') {
+      // downscroll code here
+      prevScrollDirection = 'down';
+      console.log("going down")
+      handleScrollToSection('down')
+    }
+    else if (st < prevScrollTop && prevScrollDirection !== 'up') {
+      // upscroll code
+      prevScrollDirection = 'up';
+      console.log("going up")
+      handleScrollToSection('up')
+    }
+    prevScrollTop = st <= 0 ? 0 : st; // for Mobile or negative scrolling
+  }, false);
 });
+
+
+
+
+// // Hacer scroll hacia la siguiente o anterior sección suavemente
+// if (targetElement) {
+//   targetElement.scrollIntoView({ behavior: 'smooth' });
+// }
