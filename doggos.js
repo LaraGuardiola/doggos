65eb3eb43544553dcf0c70b1b869bbdb591e47ef
Sergio Lara Guardiola <webdev.slara@gmail.com>
2023-12-24 17:04:35 +0100

    Added adjust height when resizing

--- doggos.js
+++ doggos.js
@@ -11,6 +11,13 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 // Función para ajustar la altura del contenedor .scroller
 const adjustScrollerHeight = () => {
   const scroller = document.querySelector(".scroller");
